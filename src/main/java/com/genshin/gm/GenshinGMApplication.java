1f585056cbb9c77563856294938b842bf0d31dbe
jiyu3984 <105088008@qq.com>
2026-01-03 13:02:37 +0800

    初次提交

---
+++ src/main/java/com/genshin/gm/GenshinGMApplication.java
@@ -0,0 +1,14 @@
+package com.genshin.gm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 原神 GM 指令生成系统 - 主启动类
+ */
+@SpringBootApplication
+public class GenshinGMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinGMApplication.class, args);
+    }
+}
